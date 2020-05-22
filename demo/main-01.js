0377919fdac4546ea3b1aa8bac63eb9a9220914e
shengzheng1981 <11639583@qq.com>
2020-05-22 15:39:43 +0800

    readme

--- demo/main-01.js
+++ demo/main-01.js
@@ -1,11 +1,17 @@
 import {Map} from "../dist/map.js";
 import {Point} from "../dist/geometry/point.js";
-
+import {Graphic, SimpleMarkerSymbol} from "../dist/esm2015";
 const map = new Map("foo");
-const point = new Point(100, 100);
-point.addTo(map);
-
-map.redraw();
+map.setView([116.397411,39.909186], 12);
+const marker = new SimpleMarkerSymbol();
+marker.width = 32;
+marker.height = 32;
+marker.offsetX = 16;
+marker.offsetY = 32;
+marker.url = "assets/img/marker.svg";
+const point = new Point(116.397411,39.909186);
+const graphic = new Graphic(point, marker);
+map.addGraphic(graphic);
 
 
 //cause typescript tsc forget js suffix for geometry.js
